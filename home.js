3219f50fdbae10d43c6fd8a7765b02d15133488c
Tejas <biradartejas842@gmail.com>
2025-02-07 15:22:08 +0530

    8

--- home.js
+++ home.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
             return;
         }
         if (password.length < 8) {
-            alert("Password must be at least 6 characters long.");
+            alert("Password must be at least 8 characters long.");
             return;
         }
         if (password !== repeatPassword) {
